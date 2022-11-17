5dcfd8e43a7229f4576b15b08cd3752c1ba8b54c
silviazhao <silviazhao@tencent.com>
2022-11-17 14:37:59 +0800

    @tencentcloud/chat-uikit-vue1.2.3

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    'prefer-const': 0,
+    'vue/multi-word-component-names': 0,
+  },
+};
