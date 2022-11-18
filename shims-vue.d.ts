fa41d5c7700b1dbcc8191a99e9d7ec1eace23ea3
silviazhao <silviazhao@tencent.com>
2022-11-18 19:35:28 +0800

    @tencentcloud/chat-uikit-vue1.2.4

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -1,20 +1,9 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
-
-declare module 'tim-js-sdk' {
-  import tim from 'tim-js-sdk';
-  export default tim;
-}
-
-declare module 'tim-upload-plugin' {
-  import TIMUploadPlugin from 'tim-upload-plugin';
-  export default TIMUploadPlugin;
-}
-
 declare module 'tuicall-engine-webrtc' {
   import { TUICallEngine, TUICallEvent, TUICallType } from 'tuicall-engine-webrtc';
   export { TUICallEngine, TUICallEvent, TUICallType };
@@ -24,3 +13,6 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+
+declare module 'aegis-web-sdk';
+
